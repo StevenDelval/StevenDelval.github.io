bc206ca3d2374a95bdf487cc8c8c239dab7c35e1
StevenDsimplon <asteven59220@free.fr>
2022-04-04 13:04:09 +0200

    Modif snow +navbar

--- js/snow.js
+++ js/snow.js
@@ -1,20 +1,23 @@
 const cloud=document.querySelector('.cloud');
 let rest=document.getElementsByClassName('fa-snowflake');
-setInterval(snowfall,10)
+setInterval(snowfall,50);
+
+
 function snowfall(){
     const snowflake= document.createElement('i');
     snowflake.classList.add('fa-regular');
     snowflake.classList.add('fa-snowflake');
-    snowflake.style.fontSize= Math.random() * 20 +'px';
-    snowflake.style.animationDuration= Math.random()* 1.5 +'s';
-    snowflake.style.opacity = Math.min(Math.random(),0.4);
+    snowflake.style.fontSize= Math.max((Math.random()*20),10)+'px';
+    let a=Math.random()* 2;
+    snowflake.style.animationDuration= a +'s';
+    snowflake.style.opacity = Math.max(Math.random(),0.4);
     snowflake.style.left = Math.random() * window.innerWidth + 'px';
 
     cloud.appendChild(snowflake);
 
     setTimeout(() => {
         snowflake.remove();
-    }, 1500);
+    }, a*1000);
 }
 
 
