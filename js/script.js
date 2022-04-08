f38f7276df5f577b26fa910d86734ddc5847a28d
StevenDsimplon <asteven59220@free.fr>
2022-04-08 12:31:07 +0200

    Page portfolio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 let portfolio = document.querySelector(".portfolio");
-console.log(portfolio.children)
+
 portfolio.addEventListener("click", (e) => {
-  console.log(e);
-  console.log(e.target.classList);
   if (e.target.classList[0] == "image") {
     for (let notact of portfolio.children) {
       notact.classList.remove("active")
