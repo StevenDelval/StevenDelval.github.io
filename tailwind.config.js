e2f2004a161d48c9bd6853d95a09e5434bbf800c
StevenDelval <asteven59220@free.fr>
2022-07-12 11:46:33 +0200

    Add git ignore and tailwind conjif

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+
+  theme: {
+    extend: {
+      colors: {
+        'bordeaux': '#6D071A',
+        'black-gray':'#1C1C1C',
+        'linkedin':'#0e76a8',
+      },
+    }
+  }
+  ,
+  plugins: [],
+}
